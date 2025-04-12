7d65758fe0e42c57ac27287e4b46d71109bc7bd1
linmunlok <linml-wp23@student.tarc.edu.my>
2025-04-12 21:02:02 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "employee.cwfrvpetpc58.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "linmunlok-employee"
+customregion = "us-east-1a"
+
